a434bcf1d21b79241ee654c6de87889d9337a91a
Hai Nguyen Dang <zelvor1307@gmail.com>
2022-03-16 20:14:31 +0700

    chua xong ti nao ca

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Product from "./pages/Product/Product";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/Product" element={<Product />} />
+          <Route path="/ProductDetail" element={<ProductDetail />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
