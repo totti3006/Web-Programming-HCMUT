8abc9e81575d771559f61facf4fe91c414e220cd
Thinh Nguyen <thinhnguyen.ndcs@gmail.com>
2022-04-05 06:57:36 +0700

    .

--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,13 +1,27 @@
 import { useEffect, useState } from 'react';
-import './Home.css'
-import Slider from '../../components/Home/Slider/Slider'
-import Content from '../../components/Home/Content/Content'
+import './Home.css';
+import Slider from '../../components/Home/Slider/Slider';
+import Content from '../../components/Home/Content/Content';
+import axios from 'axios';
+
 
 const Home = () => {
 
     const [className, setClassName] = useState("home-container")
 
     useEffect(() => {
+        const login = async () =>{
+            const data = {
+                username: 'thinhnguyen',
+                password: '123456'
+            }
+            await axios.post('http://localhost/ltw-api/users/login', data).then(res => {
+                console.log(res);
+            })
+        }
+
+        login();
+
         const handleScroll = () => {
             const sliderHeight = document.querySelector(".slider").clientHeight
             if(window.scrollY >= 0.6*sliderHeight) {
